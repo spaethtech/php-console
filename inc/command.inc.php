19951c8437a392466c81be72c959ea0bbf5b2405
Ryan Spaeth <rspaeth@spaethtech.com>
2022-08-05 23:59:06 -0500

    Starting changes to base commands.

--- inc/command.inc.php
+++ inc/command.inc.php
@@ -1,5 +1,6 @@
 <?php
 declare(strict_types=1);
+
 /**
  * IMPORTANT: All custom commands should "require" this script.
  *
@@ -11,21 +12,28 @@ declare(strict_types=1);
 if (php_sapi_name() !== "cli")
     exit;
 
-$IDE = __DIR__."/..";
+echo PROJECT_DIR;
+exit;
+
+//$IDE = __DIR__."/..";
+if (!($ide = realpath( PROJECT_DIR ."/ide"))
+{
+ echo "Test;"
+}
 
 // IF the IDE subsystem dependencies are not installed...
-if (!file_exists($IDE."/vendor"))
+if (!file_exists($ide."/vendor"))
 {
     // ...THEN change to the correct folder and install them!
     $owd = getcwd();
-    chdir($IDE);
-    exec("composer --ansi install", $output);
+    chdir($ide);
+    exec("composer --ansi install");
     chdir($owd);
 }
 
-require_once $IDE."/vendor/autoload.php";
+require_once $ide."/vendor/autoload.php";
 
 // Remove our unused globally scoped variables to prevent pollution.
-unset($IDE, $owd);
+unset($ide, $owd);
 
 # IMPORTANT: Only now are our globals available!
--- src/Console/Commands/BaseCommand.php
+++ src/Console/Commands/BaseCommand.php
@@ -5,7 +5,7 @@ namespace SpaethTech\Commands;
 
 use ReflectionClass;
 use ReflectionException;
-use SpaethTech\Support\FileSystem;
+use SpaethTech\Common\FileSystem;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
